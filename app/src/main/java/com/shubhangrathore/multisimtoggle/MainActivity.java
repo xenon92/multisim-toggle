19843c4def1ff30907f09434423eb9a140ef33aa
Shubhang Rathore <shubhang92@gmail.com>
2014-07-27 23:40:55 +0530

    Add Copyright and License
    GPL v3

--- app/src/main/java/com/shubhangrathore/multisimtoggle/MainActivity.java
+++ app/src/main/java/com/shubhangrathore/multisimtoggle/MainActivity.java
@@ -1,3 +1,24 @@
+/*
+ * MultiSIM Toggle
+ *
+ * Android app to enable or disable multiSIM functionality on multiSIM supporting ROMs
+ *
+ * Copyright (c) 2014 Shubhang Rathore
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.shubhangrathore.multisimtoggle;
 
 import android.app.Activity;
@@ -9,8 +30,6 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
