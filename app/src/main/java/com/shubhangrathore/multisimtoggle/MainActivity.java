3854810c3012a8d040967cfcdc48864b4fb4c4cb
Shubhang Rathore <shubhang92@gmail.com>
2014-07-27 23:32:09 +0530

    Remove options menu from the app

--- app/src/main/java/com/shubhangrathore/multisimtoggle/MainActivity.java
+++ app/src/main/java/com/shubhangrathore/multisimtoggle/MainActivity.java
@@ -42,26 +42,6 @@ public class MainActivity extends Activity {
         getCurrentMultiSimStatus();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_exit) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     private void setAppVersion() {
         try {
 
